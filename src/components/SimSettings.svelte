972f1af851af545aadbd8625d94ef51013806cd0
Nate Henty <nate.henty99@gmail.com>
2021-10-02 11:57:20 -0500

    added input boxes

---
+++ src/components/SimSettings.svelte
@@ -0,0 +1,19 @@
+<script>
+	let observer_range = 0;
+	let observer_period = 0;
+    let observee_range = 0;
+    let observee_period = 0;
+</script>
+
+<form><label>
+    Observer Range
+	<input type=number bind:value={observer_range} min=0><br>
+    Observer Period
+	<input type=number bind:value={observer_period} min=0><br>
+    Observer Range
+	<input type=number bind:value={observee_range} min=0><br>
+    Observee Period
+	<input type=number bind:value={observee_period} min=0><br>
+</label></form>
+
+
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,9 @@
 <script>
-	import STLGrabber from '../components/STL_File_Grabber.svelte';
+	import STLGrabber from '../components/STLFileGrabber.svelte';
+    import SimSettings from '../components/SimSettings.svelte';
 </script>
 
 <h1>Welcome to jo</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<STLGrabber />
+<p>   <STLGrabber /></p>
+<p> <SimSettings/></p>
