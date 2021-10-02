5294b791694524096206d1b8ff1d1d2245c9a349
EdwinEstep <edwin.estep@gmail.com>
2021-10-02 17:01:14 -0500

    Actually added the chart

---
+++ src/components/ChartJSPlot.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { onMount } from 'svelte';
+	export let inputData = [12, 19, 3, 5, 2, 3];
+
+	let range = (n) => Array.from(Array(n).keys());
+
+	onMount(() => {
+		let myChart = new Chart('myChart', {
+			type: 'scatter',
+			data: {
+				labels: range(inputData.length),
+				datasets: [
+					{
+						label: '# of Votes',
+						data: inputData,
+						borderWidth: 1
+					}
+				]
+			},
+			options: {
+				scales: {
+					y: {
+						beginAtZero: true
+					}
+				}
+			}
+		});
+	});
+</script>
+
+<svelte:head>
+	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</svelte:head>
+
+<canvas id="myChart" width="400" height="400" />
