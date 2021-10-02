d0cf94b46399fdbe9806b901a543ec26c9a2bea1
EdwinEstep <edwin.estep@gmail.com>
2021-10-02 14:20:50 -0500

    Added basic horizontal bar graph

---
+++ src/components/D3Plot.svelte
@@ -0,0 +1,38 @@
+<head>
+    <script src="https://d3js.org/d3.v4.min.js"></script>
+</head>
+
+<script>
+	import { onMount } from 'svelte';
+	import * as d3 from 'd3';
+	var data = [30, 86, 168, 281, 303, 365];
+	
+	let el;
+
+	onMount(() => {
+		d3.select(el)           /*Select the DOM element (chart) bound to el*/
+			.selectAll("div")   /*Select all "div" css sections within the chart*/
+			.data(data)
+			.enter()
+			.append("div")
+			.style("width", function(d) {
+				return (d*2) + "px";
+			})
+			.text(function(d) {
+				return d;
+			});
+	});
+</script>
+
+<style>
+	.chart :global(div) {       /*chart is the name of the graph style*/
+		font: 10px sans-serif;
+		background-color: steelblue;
+		text-align: right;
+		padding: 3px;
+		margin: 1px;
+		color: white;
+	}
+</style>
+
+<div bind:this={el} class="chart"></div>
