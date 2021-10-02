267b63207db979017bdd68d7046e68d9ab9cdab5
EdwinEstep <edwin.estep@gmail.com>
2021-10-02 14:14:42 -0500

    Added basic chart to test D3

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,13 @@
+<script>
+	import Plot from '../components/D3Plot.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Plot />
+
+<svg width="500" height="1000">
+    <circle cx="200" cy="200" r="100"
+    style="fill:red;stroke:black;stroke-width:5;opacity:0.5" />
+</svg>
