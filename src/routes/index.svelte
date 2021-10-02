8d0d44bcdd274f85d6cbbb69f83b53a57f28963c
Sam LaGrave <srl0011@uah.edu>
2021-10-02 11:41:15 -0500

    three js visualizer added

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,10 @@
 <script>
 	import ThreeSimulation from '../components/ThreeSimulation.svelte';
+	import ThreeVisualizer from '../components/ThreeVisualizer.svelte';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<ThreeSimulation color={0xff0000} width={500} height={500} />
+<ThreeSimulation width={500} height={500} />
+<ThreeVisualizer width={500} height={500} />
