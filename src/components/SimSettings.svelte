5457120b0f9875767e3774da11a788a54bd863eb
Nate Henty <nate.henty99@gmail.com>
2021-10-02 15:02:20 -0500

    sam sucks

--- src/components/SimSettings.svelte
+++ src/components/SimSettings.svelte
@@ -1,19 +1,62 @@
 <script>
-	let observer_range = 0;
-	let observer_period = 0;
-    let observee_range = 0;
-    let observee_period = 0;
+    let files;
+    export let _mythingamajig = {
+        observer_range: 0,
+        observer_period: 0,
+        obervee_range: 0,
+        observee_period: 0,
+        filename: ""
+    }
+	
+
+	$: if (files) {
+		// Note that `files` is of type `FileList`, not an Array:
+		// https://developer.mozilla.org/en-US/docs/Web/API/FileList
+		console.log(files);
+
+		for (const file of files) {
+			alert(`${file.name}: ${file.size} bytes`);
+		}
+	}
+    let simulation = { run: false };
+
+    function toggle() {
+        simulation.run = !simulation.run;
+    }
+    
 </script>
 
-<form><label>
+<label for="avatar">Upload STL File:</label>
+<input accept=".stl" bind:files id="avatar" name="avatar" type="file" />
+
+{#if files}
+	<h2>Selected files:</h2>
+	{#each Array.from(files) as file}
+		<p>{file.name} ({file.size} bytes)</p>
+	{/each}
+{/if}
+
+<form>
     Observer Range
-	<input type=number bind:value={observer_range} min=0><br>
+	<input type=number bind:value={_mythingamajig.observer_range} min=0 max=Number.MAX_SAFE_INTEGER> km<br>
     Observer Period
-	<input type=number bind:value={observer_period} min=0><br>
-    Observer Range
-	<input type=number bind:value={observee_range} min=0><br>
+	<input type=number bind:value={_mythingamajig.observer_period} min=0 max=Number.MAX_SAFE_INTEGER> years<br>
+    Observee Range
+	<input type=number bind:value={_mythingamajig.observee_range} min=0 max=Number.MAX_SAFE_INTEGER> km<br>
     Observee Period
-	<input type=number bind:value={observee_period} min=0><br>
-</label></form>
+	<input type=number bind:value={_mythingamajig.observee_period} min=0 max=Number.MAX_SAFE_INTEGER> years<br>
+</form>
 
+{#if simulation.run}
+	<button on:click={toggle}>
+		Stop
+	</button>
+    <!-- Use to start sim with inputs. -->
+{/if}
 
+{#if !simulation.run}
+	<button on:click={toggle}>
+		Run
+	</button>
+    <!-- Use to kill sim. -->
+{/if}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,10 @@
 <script>
-	import STLGrabber from '../components/STLFileGrabber.svelte';
     import SimSettings from '../components/SimSettings.svelte';
+    import {_mythingamajig} from '../components/SimSettings.svelte';
 </script>
 
-<h1>Welcome to jo</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>   <STLGrabber /></p>
-<p> <SimSettings/></p>
+<h1>Welcome to jo ¯\_(ツ)_/¯</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation.</p>
+<br>
+<SimSettings/>
+<p> Test: {_mythingamajig}</p>
