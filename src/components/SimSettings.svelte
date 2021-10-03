19986e17db623e14713e1f063db74cbd101ca1cf
Nate Henty <nate.henty99@gmail.com>
2021-10-03 11:39:44 -0500

    added more file options to drop down

--- src/components/SimSettings.svelte
+++ src/components/SimSettings.svelte
@@ -16,6 +16,14 @@
 	<option value="kleo.obj">216 Kleopatra</option>
 	<option value="betulia.obj">1580 Betulia</option>
 	<option value="geographos.obj">1620 Geographos</option>
+	<option value="1996hw1.obj">1996 Hw1</option>
+	<option value="bacchus.obj">Bacchus</option>
+	<option value="castalia.obj">Castalia</option>
+	<option value="golevka.obj">Golevka</option>
+	<option value="hirestoutatis.obj">Toutatis</option>
+	<option value="ky26.obj">Ky26</option>
+	<option value="Mithra.v1.PA.prograde.mod.obj">Mithra</option>
+	<option value="MKIII.obj">MKIII Spacesuit</option>
 </select>
 
 <form>	
