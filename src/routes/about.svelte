59f21ba188f54ee59b8061afbdfadeb53fded351
Sam LaGrave <srl0011@uah.edu>
2021-10-03 14:26:16 -0500

    about done

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,2 +1,18 @@
-<h1>About our team</h1>
-<p>Head empty, no thoughts</p>
+<h1 class="text-4xl text-center my-4">
+	Who's <i>jo</i>?
+</h1>
+
+<p class="my-8 text-center">
+    Space Apps is an event NASA hosts every year in which participants & their teams spend a weekend scrambling to write code to complete a challenge.
+    <br> Team Sam Sucks (us) selected the <a class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href="https://2021.spaceappschallenge.org/challenges/statements/when-light-curves-throw-us-curve-balls/details">light curve challenge</a>.
+    <br> In order to fulfill the requirements, the team developed a web-app which produces light curves from Earth's perspective for objects orbiting the sun.
+    <br> The app allows a user to select a model from a drop down menu, enter generic settings for the simulation including orbital period and the radius
+    <br> of orbit (using simplified, circular orbits) as well as the rotational speed of the model, and finally scaling settings for the simulation window.
+    <br> A simulation is produced and displayed. The light intensity from the object is calculated based on pixel brightness,
+    <br> and is made available for the user to plot and view at any point during the sim.
+</p>
+
+
+<h2 class="text-2xl text-center my-4 underline text-blue-600 hover:text-blue-800 visited:text-purple-600">
+	<a href="https://github.com/SLaGrave/spaceapps2021">Github Repo</a>
+</h2>
