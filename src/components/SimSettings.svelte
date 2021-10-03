c70af8e3a5176302f9dfa2445508516c3540763c
Sam LaGrave <srl0011@uah.edu>
2021-10-03 13:23:14 -0500

    Scale and labels

--- src/components/SimSettings.svelte
+++ src/components/SimSettings.svelte
@@ -4,20 +4,21 @@
 		observer_period: 1,
 		observee_distance: 2,
 		observee_period: 0.5,
+		observee_scale: 1,
 		vr: 360,
 		xbound: 500,
 		ybound: 500,
-		filename: 'kleo.obj'
+		filename: 'Mithra.v1.PA.prograde.mod.obj'
 	};
 </script>
 
-<div class="w-full max-w-xs">
+<div class="w-full max-w-md">
 	<form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
 		<label class="block text-gray-700 text-sm font-bold mb-2" for="filename"
 			>Select Object Model:</label
 		>
 		<select name="filename" id="filename" bind:value={settings.filename}>
-			<option value="kleo.obj">216 Kleopatra</option>
+			<option value="Mithra.v1.PA.prograde.mod.obj">4486 Mithra</option>
 			<option value="betulia.obj">1580 Betulia</option>
 			<option value="geographos.obj">1620 Geographos</option>
 			<option value="1996hw1.obj">1996 Hw1</option>
@@ -25,7 +26,7 @@
 			<option value="castalia.obj">4769 Castalia</option>
 			<option value="golevka.obj">6489 Golevka</option>
 			<option value="hirestoutatis.obj">4179 Toutatis</option>
-			<option value="Mithra.v1.PA.prograde.mod.obj">4486 Mithra</option>
+			<option value="kleo.obj">216 Kleopatra</option>
 			<option value="MKIII.obj">MKIII Spacesuit</option>
 		</select>
 
@@ -49,7 +50,7 @@
 			min="0.1"
 			max="Number.MAX_SAFE_INTEGER"
 		/>
-		years
+		Year(s)
 		<label class="block text-gray-700 text-sm font-bold mb-2" for="observee_distance"
 			>Observee Distance</label
 		>
@@ -71,7 +72,18 @@
 			min="0.1"
 			max="Number.MAX_SAFE_INTEGER"
 		/>
-		years
+		Year(s)
+		<label class="block text-gray-700 text-sm font-bold mb-2" for="observee_scale"
+			>Observee Scale</label
+		>
+		<input
+			name="observee_scale"
+			type="number"
+			bind:value={settings.observee_scale}
+			min="0.001"
+			max="Number.MAX_SAFE_INTEGER"
+		/>
+		(Basec on Model Size)
 		<label class="block text-gray-700 text-sm font-bold mb-2" for="vr">Rotational Velocity</label>
 		<input
 			name="vr"
@@ -80,12 +92,12 @@
 			min="0.01"
 			max="Number.MAX_SAFE_INTEGER"
 		/>
-		deg/day
+		Deg/Day (Earth)
 		<label class="block text-gray-700 text-sm font-bold mb-2" for="xbound">Simulation xbound</label>
 		<input name="xbound" type="number" bind:value={settings.xbound} min="200" max="2000" />
-		pixels
+		Pixels
 		<label class="block text-gray-700 text-sm font-bold mb-2" for="ybound">Simulation ybound</label>
 		<input name="ybound" type="number" bind:value={settings.ybound} min="200" max="2000" />
-		pixels
+		Pixels
 	</form>
 </div>
